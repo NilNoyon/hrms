0ed41c18411be4b6a8272735a5ab421ebb5e9936
joshef0196 <hossainjoshef@gmail.com>
2025-06-18 01:28:23 +0600

    Entry date add in Resignation, Movement resgistry

--- hr/urls.py
+++ hr/urls.py
@@ -310,6 +310,7 @@ urlpatterns = [
      path('fiscal_year/delete/<int:id>/', views.fiscal_year_delete, name='fiscal_year_delete'),
      path('ajax/fiscal-year-update-status/', views.fiscal_year_update_status, name='fiscal_year_update_status'),
      path('separation-management/', views.separation_management, name='separation_management'),
+     path('movement-registry/', views.movement_registry, name='movement_registry'),
      path('cessation/action/', views.employee_cessation_action, name='employee_cessation_action'),
      path('promotion-demotion/', views.promotion_demotion, name='promotion_demotion'),
      path('promotion-history-view/', views.promotion_history_view, name='promotion_history_view'),
