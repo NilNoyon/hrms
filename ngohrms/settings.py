ed346ed9bc6d27c87f1d363536460470b32607b3
noyon-sqr <noyon@esquire.com.bd>
2025-07-21 21:04:55 +0600

    noyon : issue : notification module comment out due to migrations

--- ngohrms/settings.py
+++ ngohrms/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django_countries',
-    'notification',
+    # 'notification',
     'general',
     'desk',
     'ngohrms',
